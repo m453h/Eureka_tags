09cf04a1c9bae28adefa9dbcf7912a8beb3b99ba
Michael Hudson <mnkotagu@gmail.com>
2023-09-07 09:36:30 +0300

    Implement search by selected tags

--- src/web/user/dashboard.py
+++ src/web/user/dashboard.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
-from flask import render_template, Blueprint, flash, redirect, url_for
+import requests
+from flask import render_template, Blueprint, flash, redirect, url_for, request
 from flask_login import current_user
 from sqlalchemy import desc
 
 from src import db
 from src.models.post import Post
+from src.models.tag import Tag
 from src.web.user.forms import PostForm
 
 dashboard_pages = Blueprint('dashboard_pages', __name__,
                             template_folder='templates', url_prefix='/dashboard')
 
 
-@dashboard_pages.route('/', strict_slashes=False, methods=['GET','POST'])
+@dashboard_pages.route('/', strict_slashes=False, methods=['GET', 'POST'])
 def index():
     form = PostForm()
+    tag = request.args.to_dict().get('t')
+
     if form.validate_on_submit():
         post = Post(title=form.title.data, content=form.content.data, user_id=current_user.id,
                     is_public=form.is_public.data)
@@ -26,6 +30,14 @@ def index():
         flash('Your post has been created!', 'success')
         return redirect(url_for('dashboard_pages.index'))
 
-    my_posts = Post.query.filter_by(user_id=current_user.id).order_by(desc(Post.date_created)).all()
+    post_query = Post.query.\
+        filter_by(user_id=current_user.id).\
+        order_by(desc(Post.date_created))
+
+    if tag is not None:
+        post_query = post_query.join(Post.tags) \
+            .filter(Tag.name == tag) \
+
+    my_posts = post_query.all()
 
     return render_template('dashboard/index.html', form=form, my_posts=my_posts)
