f1972aa18f214234d4331ced5f6c249916627b07
Michael Hudson <mnkotagu@gmail.com>
2023-09-12 11:34:13 +0300

    Improve application UI components

--- src/web/common.py
+++ src/web/common.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+from datetime import datetime
+
 from flask import render_template, Blueprint, redirect, url_for
 from flask_login import current_user
 
+from src import app
 
 common_pages = Blueprint('common_pages', __name__,
                          template_folder='templates')
@@ -14,3 +17,8 @@ def index():
         return redirect(url_for('dashboard_pages.index'))
 
     return render_template('common_pages/index.html')
+
+
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
